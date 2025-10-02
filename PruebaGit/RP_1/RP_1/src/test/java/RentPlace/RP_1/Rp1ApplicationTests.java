40cc98f969f8195a64c9059aaef307cf7b64962a
Pablo S <pablo.sanchez39@alu.uclm.es>
2025-10-02 16:01:59 +0200

    Prueba Pablo S

---
+++ PruebaGit/RP_1/RP_1/src/test/java/RentPlace/RP_1/Rp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package RentPlace.RP_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
